f0807594de641fd99c8adfa0f3a3b015980ec018
Romanticoseu <1251448395@qq.com>
2022-04-25 01:42:15 +0800

    v1.0

--- mapreduce.c
+++ mapreduce.c
@@ -5,6 +5,7 @@
 #include <sys/stat.h>
 #include "mapreduce.h"
 
+#define MAXNPAIRS 1024
 // Define the data structure and variable used
 
 // pairs
@@ -18,12 +19,12 @@ struct pairs
 struct pairs** partitions;
 
 // files
-struct  files
+struct  file
 {
     char *name;
 };
 
-struct files* fileNmaes;
+struct file* fileNmaes;
 
 // array for counting partitions
 int* pairCountInPartition;
@@ -108,4 +109,192 @@ int compare(const void* pairs1, const void* pairs2)
     return strcmp(p1->key, p2->key);
 }
 
-//      Sort files by increasing
+//      Sort files by increasing size
+int compareFiles(const void *pairs1,const void *pairs2)
+{
+    struct file *f1 = (struct file*)pairs1;
+    struct file *f2 = (struct file*)pairs2;
+
+    struct stat st1,st2;
+    stat(f1->name, &st1);
+    stat(f2->name, &st2);
+    long int size1 = st1.st_size;
+    long int size2 = st2.st_size;
+    
+    return (size1 - size2);
+}
+
+void MR_Emit(char *key, char *value)
+{
+    pthread_mutex_lock(&lock);
+
+    // Get partition_number
+    unsigned long hashPartitionNumber = p(key, numberPartitions);
+    pairCountInPartition[hashPartitionNumber]++;
+    int curCount = pairCountInPartition[hashPartitionNumber];
+    // Checking if allocated memory has been exceed
+    if(curCount > pairAllocatedInPartition[hashPartitionNumber])
+    {
+        pairAllocatedInPartition[hashPartitionNumber] *= 2;
+        partitions[hashPartitionNumber] = (struct pairs *)
+        realloc(partitions[hashPartitionNumber], pairAllocatedInPartition[hashPartitionNumber] * sizeof(struct pairs));
+    }
+    partitions[hashPartitionNumber][curCount - 1].key = (char *)malloc((strlen(key) + 1) * sizeof(char));
+    strcpy(partitions[hashPartitionNumber][curCount - 1].key, key);
+    partitions[hashPartitionNumber][curCount - 1].value = (char *)malloc((strlen(value) + 1) * sizeof(char));
+    strcpy(partitions[hashPartitionNumber][curCount - 1].value, value);
+
+    pthread_mutex_unlock(&lock);
+}
+
+void Init(
+    Mapper map, int num_mappers, 
+    Reducer reduce, int num_reducers, 
+    Partitioner partition, int fileNumbers,
+    int *arrayPosition)
+{
+    p = partition;
+    m = map;
+    r = reduce;
+    numberPartitions = num_reducers;
+    partitions = (struct pairs**)malloc(num_reducers * sizeof(struct pairs *));
+    fileNmaes = (struct file*)malloc(fileNumbers * sizeof(struct file));
+    pairCountInPartition = (int *)malloc(num_reducers * sizeof(int));
+    pairAllocatedInPartition = (int *)malloc(num_mappers * sizeof(int));
+    numberOfAccessInPartition = (int *)malloc(num_mappers * sizeof(int));
+
+    filesProcessed = 0;
+    totalFiles = -1;
+
+    // Initialising the arrays needed to store the key value pairs in the partitions
+    for(int i = 0; i < num_mappers; i++)
+    {
+        partitions[i] = (struct pairs *)malloc(MAXNPAIRS * sizeof(struct pairs));
+        pairCountInPartition[i] = 0;
+        pairAllocatedInPartition[i] = 1024;
+        arrayPosition[i] = i;
+        numberOfAccessInPartition[i] = 0;
+    }
+
+
+}
+
+void SortFiles(int fileNumber, char *FileNameArray[])
+{
+    for(int i = 0; i < fileNumber; i++)
+    {
+        fileNmaes[i].name = (char *)malloc(strlen(FileNameArray[i] + 1) * sizeof(char));
+        strcpy(fileNmaes[i].name, FileNameArray[i]);
+    }
+
+    qsort(&fileNmaes[0], fileNumber - 1, sizeof(struct file), compareFiles);
+}
+
+void SortPartitons(int partitionNUmber)
+{
+    for(int i = 0; i < partitionNUmber; i++)
+    {
+        qsort(partitions[i], pairCountInPartition[i], sizeof(struct pairs), compare);
+    }
+
+}
+void MR_Run(int argc, char *argv[],
+            Mapper map, int num_mappers,
+            Reducer reduce, int num_reducers,
+            Partitioner partition)
+{
+    if(argc - 1 < num_mappers) {
+        num_mappers = argc - 1;
+    }
+
+    // Initialising all variables
+    pthread_t mapThreads[num_mappers];
+    pthread_t reduceThreads[num_reducers];
+    pthread_mutex_init(&lock, NULL);
+    pthread_mutex_init(&fileLock, NULL);
+    int arrayPosition[num_reducers];
+
+    Init(map, num_mappers, reduce, num_reducers, partition, argc - 1, arrayPosition);
+
+    SortFiles(argc - 1, argv+1);
+
+    // Creating the threads for the number of mappers
+    for(int i = 0; i < num_mappers; i++)
+    {
+        pthread_create(&mapThreads[i], NULL, Map_thread, NULL);
+    }
+
+    // Waiting for threads to merge
+
+    for(int i = 0; i < num_mappers; i++)
+    {
+        pthread_join(mapThreads[i], NULL);
+    }
+
+    SortPartitions(num_reducers);
+
+    // Creating the threads for the number of reducers
+    for(int i = 0; i < num_reducers; i++)
+    {
+        // if (pthread_create(&reduceThreads[i], NULL, Reduce_thread, &arrayPosition[i]))
+        // {
+        //     printf("Error!\n");
+        // }
+        pthread_create(&reduceThreads[i], NULL, Reduce_thread, &arrayPosition[i]);
+    }
+
+    // Waiting for the threads to merge
+    for(int i = 0; i < num_reducers; i++)
+    {
+        pthread_join(reduceThreads[i], NULL);
+    }
+
+    //MapReduce End
+
+    // Destory lock
+    pthread_mutex_destroy(&lock);
+    pthread_mutex_destroy(&fileLock);
+
+    // Freeing partitions
+    for(int i = 0; i < num_mappers; i++)
+    {
+        // Freeing pairs
+        for(int j = 0; j < pairCountInPartition[i]; j++)
+        {
+            if(partitions[i][j].key != NULL || partitions[i][j].value != NULL)
+            {
+                free(partitions[i][j].key);
+                free(partitions[i][j].value);
+            }
+        }
+
+        free(partitions[i]);
+    }
+
+    // Freeing filenames
+    for(int i = 0; i < argc - 1; i++)
+    {
+        free(fileNmaes[i].name);
+    }
+
+    // Freeing memory
+    free(partitions);
+    free(fileNmaes);
+    free(pairCountInPartition);
+    free(pairAllocatedInPartition);
+    free(numberOfAccessInPartition);
+}
+
+// Default hash function
+
+unsigned long MRDefaultHashPartition(char *key, int num_partitions)
+{
+    unsigned long hash = 5381;
+    int c;
+    while((c = *key++) != '\0')
+    {
+        hash = hash*33 + c;
+    }
+
+    return hash % num_partitions;
+}
